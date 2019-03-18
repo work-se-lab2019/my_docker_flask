f094e84340f1fe2dfa1d6d28079d7a802b9b89ff
varun karandikar <varjk30@gmail.com>
2019-03-18 12:06:48 +0530

    Added zip

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
 # app.py - a minimal flask api using flask_restful
 from flask import Flask
-from flask_restful import Resource, Api
-
 app = Flask(__name__)
-api = Api(app)
-
-class HelloWorld(Resource):
-    def get(self):
-        return "<html>{'hello': 'world'}</html>"
-
-api.add_resource(HelloWorld, '/')
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+ 
+@app.route("/")
+def hello():
+	return "Hello World!"
+ 
+if __name__ == "__main__":
+	app.run()
