e994d5f14f176d8a330d8ccc99e9994bcaf84b4d
Varun Karandikar <varjk30@gmail.com>
2019-03-29 15:24:41 +0530

    Removed tarball and modified app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def hello():
-	return "Hello Krishna, Varun, Manasa and Swati!"
+	return "Hello Krishna, Varun and Swati!"
  
 if __name__ == "__main__":
 	app.run()
